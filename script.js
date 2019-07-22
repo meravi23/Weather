a8a7c3b876283dd657762484acacff65f238a6d6
Merav <merav.mi@gmail.com>
2019-07-22 14:50:49 +0300

    retrieve weather forecast for location by coordinates

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 
     // find user's location
-    var coords;
     navigator.geolocation.getCurrentPosition(callback, error);
 
     function callback(position) {
@@ -31,8 +30,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
                     for (let i = 0; i < results.length; i++) {
                         //console.log("result " + i + JSON.stringify(results[i]));
-                        //console.log(results[i].types);
-                        //console.log(results[i].formatted_address);
+                        console.log(results[i].types);
+                        console.log(results[i].formatted_address);
 
                         if (results[i].types[0] == 'locality') {
                             var city = results[i].formatted_address;
@@ -45,6 +44,28 @@ document.addEventListener("DOMContentLoaded", function (event) {
         })
     }
 
-    document.getElementById("loc").innerHTML = getLocationName();
 
+
+    // retrieve weather forecast for location
+    function getWeatherByCoords(lat, lon) {
+        var cityName = getLocationName();
+
+        //api.openweathermap.org/data/2.5/weather?q={city name},{country code}
+        //api.openweathermap.org/data/2.5/weather?q=London,uk
+        const OPEN_WEATHER_API_KEY = "b9f348e72127c9857ab6c279058b97fa";
+        var query = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + OPEN_WEATHER_API_KEY;
+
+        fetch(query)
+            .then(function (resp) { return resp.json() }) // Convert data to json
+            .then(function (data) {
+                console.log(data);
+            })
+            .catch(function () {
+                // catch any errors
+            });
+    }
+
+    var forecast = getWeatherByCoords(lat, lon);
+    console.log(JSON.stringify(forecast));
+    
 });
