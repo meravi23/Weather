e9d848afdda47594cc300fdba17ceca31a4b662b
Merav <merav.mi@gmail.com>
2019-07-22 13:55:17 +0300

    convert coordinates to city/town/location name

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
     function callback(position) {
         localStorage.setItem("lat", position.coords.latitude);
         localStorage.setItem("lon", position.coords.longitude);
-        console.log(position.coords);
     }
 
     function error() {
@@ -15,9 +14,37 @@ document.addEventListener("DOMContentLoaded", function (event) {
     }
 
     // store user's location
-    var pos = { "latitude": localStorage.getItem("lat"), "longitude": localStorage.getItem("lon") };
+    var lat = localStorage.getItem("lat");
+    var lon = localStorage.getItem("lon");
+    var pos = { "lat": parseFloat(lat), "lng": parseFloat(lon) };
     console.log("pos: " + JSON.stringify(pos));
 
-    document.getElementById("loc").innerHTML = JSON.stringify(pos);
+
+    //convert coordinates to city/town/location name
+    function getLocationName() {
+        var geocoder = new google.maps.Geocoder;
+
+        geocoder.geocode({ 'location': pos }, function (results, status) {
+            if (status === 'OK') {
+                if (results) {
+                    console.log("results qty: " + results.length);
+
+                    for (let i = 0; i < results.length; i++) {
+                        //console.log("result " + i + JSON.stringify(results[i]));
+                        //console.log(results[i].types);
+                        //console.log(results[i].formatted_address);
+
+                        if (results[i].types[0] == 'locality') {
+                            var city = results[i].formatted_address;
+                            console.log(city);
+                            document.getElementById('loc').innerHTML = city;
+                        }
+                    }
+                }
+            }
+        })
+    }
+
+    document.getElementById("loc").innerHTML = getLocationName();
 
 });
